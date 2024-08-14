e03523bd6890c0ec57843bf752faa0189d49238f
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-08-14 01:21:36 -0400

    practice complete

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,21 @@ import React from "react";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 
+/*
+
+Hierarchy:
+
+App
+  - Header
+  - MainContainer
+    -SearchBar
+    - StockContainer
+      - Stock
+    - PortfolioContainer
+      - Stock
+
+*/
+
 function App() {
   return (
     <div>
